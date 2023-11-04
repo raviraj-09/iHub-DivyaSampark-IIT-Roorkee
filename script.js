7b5b9878e709dc4ef49d1fe30af50f81813d940b
Ravi Raj <108145514+raviraj-09@users.noreply.github.com>
2023-11-04 16:54:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function login(){
+    confirm("Are you sure? \nYou want to Log In.");
+}
+
+function msg(){
+    alert("You forgotten your password. \nAre you baklol?");
+}
+
+function redirect(){
+    alert("You are redirecting to www.scesupaul.org ");
+}
+
+ 
